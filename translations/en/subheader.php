292032b9ed4f339bd00de609a42e7e6fb9215704
jul6art <geoffreyk42@gmail.com>
2020-03-23 23:07:04 +0100

    list

---
+++ translations/en/subheader.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'actions' => [
+        'text' => 'Actions',
+    ],
+    'user' => [
+        'list' => [
+            'title' => 'Les utilisateurs',
+            'subtitle' => 'Gestion des utilisateurs de la plate-forme',
+        ],
+        'actions' => [
+            'add' => [
+                'text' => 'Ajouter un utilisateur'
+            ],
+        ],
+    ],
+];
